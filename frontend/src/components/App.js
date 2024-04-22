ea8fd9474de143819a613c37726478f3964db7f2
Shreya <shreyarb@sas.upenn.edu>
2024-04-21 23:13:39 -0400

    Added checkbox

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,23 @@
 import './App.css';
 import Login from './Login';
 
-function App() {
- return (<Login />)
+import React, { useState } from "react";
+
+const Checkbox = (props) => {
+	return <label>
+		<input type="checkbox" name={props.name} checked={props.val} onChange={() => {props.setValue(!props.val)}}/>
+		{props.label}
+	</label>
+}
+const App = () => {
+	const [val, setVal] = useState(false); //initializes checkbox to false 
+  const label = "My Checkbox!"
+  const label2 = "Second checkbox" 
+	return <>
+		<Checkbox value={val} setValue={setVal} label={label}></Checkbox>
+        <Checkbox value={val} setValue={setVal} label={label2}></Checkbox>
+        <Login />
+	</>
 }
 
 export default App;
