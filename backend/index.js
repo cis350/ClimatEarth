da2bbe6e15aa0b4e037aa92975aa78b1bd291278
ANNA VAZHAEPARAMBIL <anna.vazhaeparambil@gmail.com>
2024-05-14 01:43:43 -0400

    procfile updated

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,17 @@ const port = process.env.PORT || 5050;
 webapp.listen(port, () =>{
     console.log('Server running on port', port);
 })
+
+const allowedHosts = ['https://climatearth-app-f6f0a136cce9.herokuapp.com/', 'localhost'];
+app.enable('trust proxy');
+app.use((req, res, next) => {
+  const host = req.header('host');
+  if (allowedHosts.includes(host)) {
+    return next();
+  }
+  const newHost = allowedHosts.find(h => host.endsWith(h));
+  if (newHost) {
+    req.header('host', newHost);
+  }
+  res.status(400).send('Invalid Host header');
+});
